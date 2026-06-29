490a8314e9ff9aca12553460400b9deb37640133
fadillawani <152498300+fadillawani@users.noreply.github.com>
2026-06-29 09:20:33 +0000

    Initial commit

---
+++ badwallet-api/src/test/java/com/exam/badwallet_api/BadwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.badwallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
